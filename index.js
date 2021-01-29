18690edcd5923f6ae8866f21d0c0092249742e8b
MishaelChL <jose.mishael.chile@gmail.com>
2021-01-29 18:41:01 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { response } = require('express');
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+//Creamos el servidor
+const app = express();
+
+//Conectamos a la base de datos
+conectarDB();
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/productos', require('./routes/producto'));
+
+
+app.listen(4000, () =>{
+    console.log('El servidor está corriendo perfectamente');
+})
